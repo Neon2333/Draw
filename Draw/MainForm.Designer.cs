14aad3ba60d5bbc0bd58c3e252a135cbfc9a482b
“Wangkun19920124” <2522466153@qq.com>
2023-08-06 18:18:00 +0800

    自由绘制done

---
+++ Draw/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Draw
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_freeDraw = new System.Windows.Forms.Panel();
+            this.Button_freeDraw = new System.Windows.Forms.Button();
+            this.Button_quitFreeDraw = new System.Windows.Forms.Button();
+            this.Button_flush = new System.Windows.Forms.Button();
+            this.label_Y = new System.Windows.Forms.Label();
+            this.textBox_Y = new System.Windows.Forms.TextBox();
+            this.label_X = new System.Windows.Forms.Label();
+            this.textBox_X = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel_freeDraw
+            // 
+            this.panel_freeDraw.BackColor = System.Drawing.Color.White;
+            this.panel_freeDraw.Location = new System.Drawing.Point(12, 12);
+            this.panel_freeDraw.Name = "panel_freeDraw";
+            this.panel_freeDraw.Size = new System.Drawing.Size(1744, 1009);
+            this.panel_freeDraw.TabIndex = 0;
+            this.panel_freeDraw.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_freeDraw_MouseDown);
+            this.panel_freeDraw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_freeDraw_MouseMove);
+            this.panel_freeDraw.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel_freeDraw_MouseUp);
+            // 
+            // Button_freeDraw
+            // 
+            this.Button_freeDraw.BackColor = System.Drawing.Color.White;
+            this.Button_freeDraw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_freeDraw.Location = new System.Drawing.Point(1774, 13);
+            this.Button_freeDraw.Name = "Button_freeDraw";
+            this.Button_freeDraw.Size = new System.Drawing.Size(116, 44);
+            this.Button_freeDraw.TabIndex = 1;
+            this.Button_freeDraw.Text = "自由画线";
+            this.Button_freeDraw.UseVisualStyleBackColor = false;
+            this.Button_freeDraw.Click += new System.EventHandler(this.Button_freeDraw_Click);
+            // 
+            // Button_quitFreeDraw
+            // 
+            this.Button_quitFreeDraw.BackColor = System.Drawing.Color.White;
+            this.Button_quitFreeDraw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_quitFreeDraw.Location = new System.Drawing.Point(1774, 77);
+            this.Button_quitFreeDraw.Name = "Button_quitFreeDraw";
+            this.Button_quitFreeDraw.Size = new System.Drawing.Size(116, 49);
+            this.Button_quitFreeDraw.TabIndex = 2;
+            this.Button_quitFreeDraw.Text = "退出自由画线";
+            this.Button_quitFreeDraw.UseVisualStyleBackColor = false;
+            this.Button_quitFreeDraw.Click += new System.EventHandler(this.Button_quitFreeDraw_Click);
+            // 
+            // Button_flush
+            // 
+            this.Button_flush.BackColor = System.Drawing.Color.White;
+            this.Button_flush.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_flush.Location = new System.Drawing.Point(1774, 145);
+            this.Button_flush.Name = "Button_flush";
+            this.Button_flush.Size = new System.Drawing.Size(116, 54);
+            this.Button_flush.TabIndex = 3;
+            this.Button_flush.Text = "清空画布";
+            this.Button_flush.UseVisualStyleBackColor = false;
+            this.Button_flush.Click += new System.EventHandler(this.Button_flush_Click);
+            // 
+            // label_Y
+            // 
+            this.label_Y.AutoSize = true;
+            this.label_Y.Location = new System.Drawing.Point(1773, 266);
+            this.label_Y.Name = "label_Y";
+            this.label_Y.Size = new System.Drawing.Size(23, 15);
+            this.label_Y.TabIndex = 11;
+            this.label_Y.Text = "Y:";
+            // 
+            // textBox_Y
+            // 
+            this.textBox_Y.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox_Y.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_Y.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_Y.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.textBox_Y.Location = new System.Drawing.Point(1802, 266);
+            this.textBox_Y.Name = "textBox_Y";
+            this.textBox_Y.ReadOnly = true;
+            this.textBox_Y.Size = new System.Drawing.Size(77, 18);
+            this.textBox_Y.TabIndex = 10;
+            // 
+            // label_X
+            // 
+            this.label_X.AutoSize = true;
+            this.label_X.Location = new System.Drawing.Point(1773, 225);
+            this.label_X.Name = "label_X";
+            this.label_X.Size = new System.Drawing.Size(23, 15);
+            this.label_X.TabIndex = 9;
+            this.label_X.Text = "X:";
+            // 
+            // textBox_X
+            // 
+            this.textBox_X.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox_X.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_X.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_X.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.textBox_X.Location = new System.Drawing.Point(1802, 225);
+            this.textBox_X.Name = "textBox_X";
+            this.textBox_X.ReadOnly = true;
+            this.textBox_X.Size = new System.Drawing.Size(77, 18);
+            this.textBox_X.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.label_Y);
+            this.Controls.Add(this.textBox_Y);
+            this.Controls.Add(this.label_X);
+            this.Controls.Add(this.textBox_X);
+            this.Controls.Add(this.Button_flush);
+            this.Controls.Add(this.Button_quitFreeDraw);
+            this.Controls.Add(this.Button_freeDraw);
+            this.Controls.Add(this.panel_freeDraw);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_freeDraw;
+        private System.Windows.Forms.Button Button_freeDraw;
+        private System.Windows.Forms.Button Button_quitFreeDraw;
+        private System.Windows.Forms.Button Button_flush;
+        private System.Windows.Forms.Label label_Y;
+        private System.Windows.Forms.TextBox textBox_Y;
+        private System.Windows.Forms.Label label_X;
+        private System.Windows.Forms.TextBox textBox_X;
+    }
+}
+
---
+++ Draw/MainForm.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Draw
+{
+    public partial class MainForm : Form
+    {
+        //绘制模式
+        private enum FormMode { None, FreeDraw };
+        //当前模式
+        private FormMode currentMode;
+        //帧率
+        private int g_frameRate = 60;
+        private int g_millSecWait = 0;
+        //画布
+        private Graphics graphics;
+        Pen pen;
+        //鼠标左键按下标志
+        private bool flagLeftDown = false;
+        //鼠标右下按下标志
+        private bool flagRightDown = false;
+        //终止线程
+        private CancellationTokenSource cancelTokenSourceFreeDraw;
+        private CancellationToken cancelTokenFreeDraw;
+        //鼠标坐标
+        int pX = 0;
+        int pY = 0;
+        int pXFormer = 0;
+        int pYFormer = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            initForm();
+        }
+
+        //初始化页面
+        private void initForm()
+        {
+            initFreeDraw();
+        }
+
+        //画点函数
+        void drawCircleAndFill(int x, int y, int r, Graphics gp)
+        {
+            gp.DrawEllipse(new Pen(Color.Red, 1), x - r, y - r, (int)(1.414 * r), (int)(1.414 * r));  //r为圆点半径
+            gp.FillEllipse(new SolidBrush(Color.Red), x - r, y - r, (int)(1.414 * r), (int)(1.414 * r));
+        }
+
+        /// <summary>
+        /// 初始化自由绘制mode参数
+        /// 可以编写config等工具栏设置这些参数
+        /// </summary>
+        private void initFreeDraw()
+        {
+            cancelTokenSourceFreeDraw = new CancellationTokenSource();
+            cancelTokenFreeDraw = cancelTokenSourceFreeDraw.Token;
+            graphics = panel_freeDraw.CreateGraphics();
+            //线粗细、颜色
+            pen = new Pen(Color.Blue, 2);
+            int g_millSecWait = 1000 / g_frameRate;
+        }
+
+        /// <summary>
+        /// 自由画线模式
+        /// </summary>
+        /// <param name="xStart"></param>
+        /// <param name="yStart"></param>
+        /// <param name="pen"></param>
+        /// <param name="graphics"></param>
+        /// <param name="frameRate"></param>
+        private void FreeDraw(Pen pen, Graphics graphics, int frameRate)
+        {
+            try
+            {
+                if (currentMode == FormMode.FreeDraw)
+                {
+                    Task.Run(() =>
+                    {
+                        //进入freeDraw模式才画
+                        while (!cancelTokenSourceFreeDraw.IsCancellationRequested)
+                        {
+                            //按下左键才画
+                            //鼠标坐标改变才画线
+                            if (flagLeftDown && (pXFormer != pX || pYFormer != pY))
+                            {
+                                //连点成线
+                                //drawCircleAndFill(pXFormer, pYFormer, 2, graphics);
+                                //折线
+                                graphics.DrawLine(pen, pXFormer, pYFormer, pX, pY);
+
+                                //更新坐标（只有画过线的坐标才更新，这样不会点和点之间不会有间断）
+                                pXFormer = pX;
+                                pYFormer = pY;
+
+                                //按帧数延时(只有画了线才延迟）
+                                cancelTokenFreeDraw.WaitHandle.WaitOne(g_millSecWait);
+                            }
+                        }
+                    }, cancelTokenFreeDraw);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 鼠标移动
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel_freeDraw_MouseMove(object sender, MouseEventArgs e)
+        {
+            //更新鼠标当前位置
+            pX = e.X;
+            pY = e.Y;
+            textBox_X.Text = pX.ToString();
+            textBox_Y.Text = pY.ToString();
+        }
+
+
+        /// <summary>
+        /// 鼠标按下事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel_freeDraw_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button.Equals(MouseButtons.Left))
+            {
+                flagLeftDown = true;
+                //按下瞬间记录起点
+                pXFormer = pX;
+                pYFormer = pY;
+            }
+            else if (e.Button.Equals(MouseButtons.Right))
+            {
+                flagRightDown = true;
+                //这里添加擦除上一个曲线功能
+            }
+        }
+
+        /// <summary>
+        /// 鼠标弹起事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel_freeDraw_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button.Equals(MouseButtons.Left))
+            {
+                flagLeftDown = false;
+            }
+            else if (e.Button.Equals(MouseButtons.Right))
+            {
+                flagRightDown = false;
+            }
+        }
+
+
+        private void Button_freeDraw_Click(object sender, EventArgs e)
+        {
+            if (currentMode != FormMode.FreeDraw)
+            {
+                //自由画线模式
+                cancelTokenSourceFreeDraw = new CancellationTokenSource();
+                cancelTokenFreeDraw = cancelTokenSourceFreeDraw.Token;
+                currentMode = FormMode.FreeDraw;
+                FreeDraw(pen, graphics, g_frameRate);
+            }
+        }
+
+        /// <summary>
+        /// 退出自由画线模式
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_quitFreeDraw_Click(object sender, EventArgs e)
+        {
+            cancelTokenSourceFreeDraw.Cancel();
+            currentMode = FormMode.None;
+        }
+
+        
+        /// <summary>
+        /// 清空画布
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_flush_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+        }
+
+    }
+
+}
